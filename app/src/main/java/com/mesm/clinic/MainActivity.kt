64e46727ea77331e3c25ded2f79caffec4c0028e
mmmeeedddooo565 <alakrap@gmail.com>
2026-04-10 01:48:27 +0200

    Update MainActivity.kt

--- app/src/main/java/com/mesm/clinic/MainActivity.kt
+++ app/src/main/java/com/mesm/clinic/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
